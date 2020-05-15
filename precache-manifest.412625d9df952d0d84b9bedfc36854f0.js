637f1aafdeda4e79cfe3ac709c64f0fc73d15536
Iain Shorter <iainshorter@gmail.com>
2020-05-15 10:43:29 +0100

    Add files via upload

---
+++ precache-manifest.412625d9df952d0d84b9bedfc36854f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1866fc0fe9f92e795b8dddb399c7eab4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f4a66d5a34b24918cd3",
+    "url": "/static/css/main.d31eedb1.chunk.css"
+  },
+  {
+    "revision": "fc207032198d1789c062",
+    "url": "/static/js/2.3c5b358e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3c5b358e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f4a66d5a34b24918cd3",
+    "url": "/static/js/main.7403cc80.chunk.js"
+  },
+  {
+    "revision": "7b9fc56561b2017b0b7c",
+    "url": "/static/js/runtime-main.754aca2f.js"
+  },
+  {
+    "revision": "df16477df93548f2548a02ead2f2d0bb",
+    "url": "/static/media/Oswald-Medium.df16477d.ttf"
+  },
+  {
+    "revision": "f621c504d70317ff13774e27d643ba21",
+    "url": "/static/media/SourceCodePro-Medium.f621c504.ttf"
+  }
+]);
